909bf24febff3508b5f0dd285290b2ab471785cd
wuchau <wuchau@connect.ust.hk>
2020-01-30 13:57:01 +0800

    broken our hompage into smaller components,finished styling our menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+
+import HomePage from './pages/homepage/homepage.component';
+
 import './App.css';
-import HomePage from './homepage.component';
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
 }
+
 
 export default App;
