28d0f8981f19d928c72afa715e213e2bd967514f
wuchau <wuchau@connect.ust.hk>
2020-02-10 02:46:13 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
+import{connect} from 'react-redux';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from'./pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
+import {setCurrentUser} from './redux/user/user.actions'
 const HatsPage = () =>(
 <div>
   <h1>HatsPage</h1>
@@ -14,16 +16,11 @@ const HatsPage = () =>(
 
 );
 class App extends React.Component {
-  constructor(){
-    super();
 
-    this.state={
-      currentUser:null
-    };
-  }
   unsubscribeFromAuth= null;
 
   componentDidMount(){
+    const{setCurrentUser} = this.props;
    this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth=> {
      // this.setState({currentUser:user});
      //createUserProfileDocument(user); 
@@ -32,31 +29,29 @@ class App extends React.Component {
        const userRef = await createUserProfileDocument(userAuth);
 
        userRef.onSnapshot(snapShot => {
-         this.setState({
-          currentUser:{
+         setCurrentUser({
+      
             id:snapShot.id,
             ...snapShot.data()
-          }
+          
 
-         }, ()=>{  
-          console.log(this.state);
          }
          );
        });
       
      }
-     this.setState({currentUser:userAuth});
+     setCurrentUser(userAuth);
     });
   }
   componentWillUnmount(){
     this.unsubscribeFromAuth();
 
-  }
+  } 
 render(){
     return (
 
 <div>
-<Header currentUser={this.state.currentUser}/>
+<Header />
 <Switch>
 <Route exact path='/' component={HomePage}/>
 <Route   path='/shop' component={ShopPage}/>
@@ -67,5 +62,9 @@ render(){
   }
 
 }
+const mapDispatchToProps = dispatch=> ({
+  setCurrentUser: user=>dispatch(setCurrentUser(user))
 
-export default App;
+})
+export default connect(
+  null,mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils';
+
 class SignIn extends React.Component{
     constructor(props){
         super(props);
@@ -13,9 +14,16 @@ class SignIn extends React.Component{
         password:''
     }
 }
-handleSubmit= event =>{
+handleSubmit= async event =>{
     event.preventDefault();
-    this.setState({email:'',password:''})
+    const{email,password}=this.state;
+    try {
+        await auth.signInWithEmailAndPassword(email,password);
+        this.setState({email:'',password:''})
+    }catch(error){
+        console.log(error);
+
+    }
 }
 handleChange = event =>{
 const {value, name}= event.target;
@@ -40,7 +48,7 @@ render(){
                 type="password"
                 label="password"
                 handleChange={this.handleChange}
-                value={this.state.email} 
+                value={this.state.password} 
                 
                 required/>
                 
