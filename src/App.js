557ff565c7550221cbeb3fc44efacae7d128c433
wuchau <wuchau@connect.ust.hk>
2020-02-10 19:53:09 +0800

    redirect to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route, Redirect} from 'react-router-dom';
 import{connect} from 'react-redux';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import HomePage from './pages/homepage/homepage.component';
@@ -40,7 +40,7 @@ class App extends React.Component {
        });
       
      }
-     setCurrentUser(userAuth);
+     setCurrentUser( userAuth);
     });
   }
   componentWillUnmount(){
@@ -55,16 +55,20 @@ render(){
 <Switch>
 <Route exact path='/' component={HomePage}/>
 <Route   path='/shop' component={ShopPage}/>
-<Route   path='/signin' component={SignInAndSignUpPage}/>
+<Route  exact path='/signin' render={()=>this.props.currentUser?(<Redirect to ='/'/>):
+  (<SignInAndSignUpPage/>)}/>
 </Switch>
 </div>
     );
   }
 
 }
+const mapStateToProps=({ user })=>({
+  currentUser:user.currentUser
+})
 const mapDispatchToProps = dispatch=> ({
   setCurrentUser: user=>dispatch(setCurrentUser(user))
 
 })
 export default connect(
-  null,mapDispatchToProps)(App);
+  mapStateToProps,mapDispatchToProps)(App);
