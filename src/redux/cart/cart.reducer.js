e0966683897be39f34d667d0de3ad863d00f67d4
wuchau <wuchau@connect.ust.hk>
2020-02-19 19:59:50 +0800

    checkout

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,11 +1,12 @@
 import CartActionTypes from './cart.types';
-import {addItemToCart} from './cart.utils';
+import { addItemToCart } from './cart.utils';
+
 const INITIAL_STATE = {
   hidden: true,
-  cartItems:[]
+  cartItems: []
 };
 
-const cartReducer = (state = INITIAL_STATE, action) => { 
+const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
@@ -13,10 +14,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden
       };
     case CartActionTypes.ADD_ITEM:
-        return{
-            ...state,
-            cartItems:addItemToCart(state.cartItems,action.payload)
-        };
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
     default:
       return state;
   }
