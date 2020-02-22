b0ffb0933576e960357546765d59ce9e4997a752
wuchau <wuchau@connect.ust.hk>
2020-02-23 01:02:46 +0800

    moved our directory and shop and data into its own reducer,create respective component with ux flow form directory/shop

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart,removeItemFromCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -19,15 +19,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
     case CartActionTypes.REMOVE_ITEM:
-      return{
+      return {
         ...state,
-        cartItems: removeItemFromCart(state.cartItems,action.payload)
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       };
-    case  CartActionTypes.CLEAR_ITEM_FROM_CART:
-      return{
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
         ...state,
-        cartItems: state.cartItems.filter(cartItem => cartItem.id!=action.payload.id
-          )
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
       };
     default:
       return state;
